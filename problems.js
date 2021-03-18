79859978726b46ad505b0120e68c1c7f39ed3ce2
Richard Green <richardgreen@pursuit.org>
2021-03-17 22:07:55 -0400

    find the average not including the lowest and highest amount

--- problems.js
+++ problems.js
@@ -1202,3 +1202,26 @@ var diStringMatch = function (S) {
     output.push(high)
     return output
 };
+
+/**
+ * 
+ * Given an array of unique integers salary where salary[i] is the salary of the employee i.
+
+Return the average salary of employees excluding the minimum and maximum salary.
+
+Input: salary = [4000,3000,1000,2000]
+Output: 2500.00000
+Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+Average salary excluding minimum and maximum salary is (2000+3000)/2= 2500
+
+ */
+
+var average = function (salary) {
+    const sortedSalary = salary.sort((a, b) => a - b)
+    let sum = 0;
+    let divisable = salary.length - 2
+    for (let i = 1; i < sortedSalary.length - 1; i++) {
+        sum += sortedSalary[i]
+    }
+    return sum / divisable
+};
